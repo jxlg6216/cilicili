8cbce3449dd4cee9869dc4d18f8234dc946059ef
lowboy123 <56386363+lowboy123@users.noreply.github.com>
2020-03-11 10:35:10 +0800

    添加 eureka_client_comment 跨域访问的过滤器

--- eureka_client_comment/src/main/java/cn/rk6216/eureka_client_comment/EurekaClientCommentApplication.java
+++ eureka_client_comment/src/main/java/cn/rk6216/eureka_client_comment/EurekaClientCommentApplication.java
@@ -1,9 +1,12 @@
 package cn.rk6216.eureka_client_comment;
 
+import cn.rk6216.eureka_client_comment.filter.AccessControlAllowOriginFilter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,6 +15,18 @@ public class EurekaClientCommentApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientCommentApplication.class, args);
+    }
+
+    /**
+     * 配置跨域访问的过滤器
+     * @return
+     */
+    @Bean
+    public FilterRegistrationBean registerFilter(){
+        FilterRegistrationBean bean = new FilterRegistrationBean();
+        bean.addUrlPatterns("/*");
+        bean.setFilter(new AccessControlAllowOriginFilter());
+        return bean;
     }
 
 }
